6130151a01ad14aed5a25f79d4b173c3b4a7eebf
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-20 22:02:12 +0530

    add json format and change date structure

--- src/components/NameList/NameList.js
+++ src/components/NameList/NameList.js
@@ -3,15 +3,37 @@ import NameListItems from './NameListItems';
 import NameListItems1 from './NameListItems1';
 function NameList()
 {
+    const nameList={
+        name:{
+            title:'mr.',
+            first:'ishan',
+            last:'reshmika',
+        },
+        location:{
+            city:'Kottagoda,Matara',
+        },
+        email:'reshmikaediriweera1997@gmail.com',
+        dob:{
+            date:'1997-03-30',
+            age:24,
+        },
+
+    };
     return(
         <React.Fragment>
             <h1>Name List</h1>
             <hr/>
             <ul>
-           <NameListItems name='Ishan Reshmika' course='c'/>
-            <NameListItems1 name='Dilshan' course='c++'/>
-            <li>Ishan</li>
-            <li>Ishan</li>
+           <NameListItems 
+           name='Ishan Reshmika' 
+           course='c'
+           email='ishan@gmail.com'
+           avater=''
+           city=''    
+           />
+            <NameListItems name={nameList.name.title+' '+nameList.name.first} email={nameList.email} birth={nameList.dob.date}/>
+            <NameListItems name='Nelara' course='Java'/>
+            <NameListItems name='Ashini' course='Nodejs'/>
             </ul>
 
         </React.Fragment>
--- src/components/NameList/NameListItems.js
+++ src/components/NameList/NameListItems.js
@@ -3,7 +3,12 @@ import React from 'react';
 function NameListItems(props)
 {
     return (
-        <li>{props.name} course-{props.course}</li>
+        <li>
+            <p>{props.name}</p>
+            <p>course-{props.course}</p>
+            <p>{props.email}</p>
+            <p>Birthday-{new Intl.DateTimeFormat('en-US').format(new Date(props.birth)) }</p>
+         </li>
     );
 }
 
