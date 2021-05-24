92109e2771d0e589f05a4fb2f435a4a28f4d1898
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-24 21:22:52 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-import React from 'react';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
-import NameList from './components/pages/NameList/NameList';
-import HeaderBar from './components/HeaderBar/HeaderBar';
+// import React from 'react';
+// import {BrowserRouter,Route,Switch} from 'react-router-dom';
+// import NameList from './components/pages/NameList/NameList';
+// import HeaderBar from './components/HeaderBar/HeaderBar';
 
-import Home from './components/pages/Home/Home';
-import About from './components/pages/About/About';
+// import Home from './components/pages/Home/Home';
+// import About from './components/pages/About/About';
 
 
 
-function App() {
-  return (
-   <div>
-   <BrowserRouter>
-   <HeaderBar/>
+// function App() {
+//   return (
+//    <div>
+//    <BrowserRouter>
+//    <HeaderBar/>
 
-   <Switch>
-   <Route path='/about'><About/></Route>
-   <Route path='/nameList'> <NameList/></Route>
-   <Route path='/'><Home/></Route>
+//    <Switch>
+//    <Route path='/about'><About/></Route>
+//    <Route path='/nameList'> <NameList/></Route>
+//    <Route path='/'><Home/></Route>
    
 
-   </Switch>
+//    </Switch>
    
    
     
 
-   </BrowserRouter>
+//    </BrowserRouter>
   
-   </div>
+//    </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+
+function App()
+{
+  return(
+    <h1>Hello Ishan</h1>
   );
 }
-
 export default App;
