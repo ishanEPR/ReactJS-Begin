468cdd915fe03fa1d7b7c85378cad1fe9cc14872
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-27 18:27:17 +0530

    call api, display data using class components

--- src/components/pages/NameListC/NameListC.js
+++ src/components/pages/NameListC/NameListC.js
@@ -1,17 +1,77 @@
 import React, { Component } from 'react';
 
+import NameListItems from '../NameList/NameListItems';
+
+
 class NameListC extends Component {
 
-    constructor(){
+    constructor(props){
 
-        super();
+        super(props);
         console.log('Constructer called');
-        this.state={message:'Hello State'};
+        this.state={nameList:
+            [
+                {
+                    id:1,
+                            name:{
+                        title:'mr.',
+                        first:'ishan',
+                        last:'reshmika',
+                    },
+                    location:{
+                        city:'Kottagoda,Matara',
+                    },
+                    email:'reshmikaediriweera1997@gmail.com',
+                    dob:{
+                        date:'1997-03-30',
+                        age:24,
+                    },
+                    picture: {
+            
+                      
+                        medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
+                       
+                      },
+                },
+                {
+                    id:2,
+                    name:{
+                title:'mr.',
+                first:'ishan',
+                last:'resh',
+            },
+            location:{
+                city:'Kottagoda,Matara',
+            },
+            email:'reshmikaediriweera1997@gmail.com',
+            dob:{
+                date:'1997-03-30',
+                age:24,
+            },
+            picture: {
+            
+              
+                medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
+               
+              },
+            },
+            
+            
+            ]
+
+        };
 
     }
     componentDidMount()
     {
-        console.log('componentDidMount Method called');
+    fetch("https://randomuser.me/api").then(Response=>{
+      return Response.json();
+    }).then(ResponseData=>{
+      
+       this.setState({nameList:[...this.state.nameList,ResponseData.results[0]]});
+console.log(this.state.nameList);
+
+    })
     }
    componentDidUpdate()
    {
@@ -19,18 +79,38 @@ class NameListC extends Component {
     console.log(this.state.message);
        
    }
+   
 
    addUserHandler=()=>
    {
        console.log('button clicked');
        console.log(this.state.message);
-       this.setState({message:"Hello State Changed"});
+      
    }
+
+   nameListComponent=()=>
+   {
+   
+      
+       return (
+           this.state.nameList.map((aName)=>
+           {
+               return   <NameListItems key={aName.id} image={aName.picture.medium} name={aName.name.title+" "+aName.name.first+" "+aName.name.last} city={aName.location.city} email={aName.email} dob={aName.dob.date}/>
+              
+           })
+         
+       );
+   }
+   
+
+
+
     render() { 
         return (
            <div className='container mt-4'>
            <button className='btn btn-primary mb-2' onClick={this.addUserHandler}>Add Name</button>
            <ul className='list-group'>
+           {this.nameListComponent()}
 
            </ul>
 
