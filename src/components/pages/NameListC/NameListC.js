3245e91dfe629432ed4db3bd58da00844ba2f4c6
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-27 16:19:31 +0530

    add stages

--- src/components/pages/NameListC/NameListC.js
+++ src/components/pages/NameListC/NameListC.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class NameListC extends Component {
-   
+
+    constructor(){
+
+        super();
+        console.log('Constructer called');
+        this.state={message:'Hello State'};
+
+    }
+    componentDidMount()
+    {
+        console.log('componentDidMount Method called');
+    }
+   componentDidUpdate()
+   {
+    console.log('componentDidUpdate Method called');
+    console.log(this.state.message);
+       
+   }
+
+   addUserHandler=()=>
+   {
+       console.log('button clicked');
+       console.log(this.state.message);
+       this.setState({message:"Hello State Changed"});
+   }
     render() { 
         return (
-            <h1>{this.props.message}</h1>
+           <div className='container mt-4'>
+           <button className='btn btn-primary mb-2' onClick={this.addUserHandler}>Add Name</button>
+           <ul className='list-group'>
+
+           </ul>
+
+           </div>
         );
     }
 }
